de10349801610c071483f614fba171c4efd9fb66
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2023-05-30 14:52:54 -0400

    added styles to nav bar and created banner component

--- src/Nav.js
+++ src/Nav.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Nav.css";
 
 function Nav() {
+	const [show, handleShow] = useState(false);
+
+	const transitionNavBar = () => {
+		if (window.scrollY > 100) {
+			handleShow(true);
+		} else {
+			handleShow(false);
+		}
+	};
+
+	useEffect(() => {
+		window.addEventListener("scroll", transitionNavBar);
+		return () => window.removeEventListener("scroll", transitionNavBar);
+	}, []);
+
 	return (
-		<div className="nav">
-			<img
-				src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
-				alt="Netflix Logo"
-				className="nav__logo"
-			/>
-			<h1>This is the nav</h1>
+		<div className={`nav ${show && "nav__black"}`}>
+			<div className="nav__contents">
+				<img
+					src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+					alt="Netflix Logo"
+					className="nav__logo"
+				/>
+
+				<img
+					src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+					alt="Netflix Avatar"
+					className="nav__avatar"
+				/>
+			</div>
 		</div>
 	);
 }
