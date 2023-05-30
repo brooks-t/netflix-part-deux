f007e87626ac84b34db7413520a357abbe4126bc
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2023-05-30 13:36:05 -0400

    initial set up. added firebase. added homescreen, nav components and css files. added netlix logo to nav.

---
+++ src/Nav.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Nav.css";
+
+function Nav() {
+	return (
+		<div className="nav">
+			<img
+				src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+				alt="Netflix Logo"
+				className="nav__logo"
+			/>
+			<h1>This is the nav</h1>
+		</div>
+	);
+}
+
+export default Nav;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyBripuLfoOjCHvaKUxcadC-g4alClgCPp8",
+	authDomain: "netflix-part-deux.firebaseapp.com",
+	projectId: "netflix-part-deux",
+	storageBucket: "netflix-part-deux.appspot.com",
+	messagingSenderId: "9253868013",
+	appId: "1:9253868013:web:3092c8f40f66fc55bd17e4",
+	measurementId: "G-5Q8L9T802G",
+};
